01c4dc82c67b817f7a5fe5839a1fc315b1477c6d
Shreedevi <shrpatil@phoenix.tech>
2024-04-14 23:47:07 +0530

    Added app-routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,26 @@
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import {NgModule} from '@angular/core';
+import {RouterModule, Routes} from '@angular/router';
+import {HomeComponent} from "./home/home.component";
+import {LoginComponent} from "./login/login.component";
+import {SignupComponent} from "./signup/signup.component";
+import {ProductsComponent} from "./products/products.component";
+import {AccountComponent} from "./account/account.component";
+import {RecipesComponent} from "./recipes/recipes.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: HomeComponent},
+  {path: 'products', component: ProductsComponent},
+  {path: 'recipes', component: RecipesComponent},
+  // { path: 'contact-us', component: ContactUsComponent },
+  // { path: 'feedback', component: FeedbackComponent },
+  {path: 'account', component: AccountComponent},
+  {path: 'signup', component: SignupComponent},
+  {path: 'login', component: LoginComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+}
